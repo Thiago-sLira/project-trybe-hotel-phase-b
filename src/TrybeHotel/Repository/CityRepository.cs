92f2fb682aa8ecb5dcbc16e3a0ca4809419dbdee
Thiago Lira <thiago.slira@outlook.com>
2023-08-15 16:48:06 -0300

    <chore>: Phase A CityRepository implementation completed

--- src/TrybeHotel/Repository/CityRepository.cs
+++ src/TrybeHotel/Repository/CityRepository.cs
@@ -13,12 +13,22 @@ namespace TrybeHotel.Repository
 
         public IEnumerable<CityDto> GetCities()
         {
-            throw new NotImplementedException();
+            return _context.Cities.Select(c => new CityDto
+            {
+                cityId = c.CityId,
+                name = c.Name,
+            });
         }
 
         public CityDto AddCity(City city)
         {
-            throw new NotImplementedException();
+            _context.Cities.Add(city);
+            _context.SaveChanges();
+            return _context.Cities.Select(c => new CityDto
+            {
+                cityId = c.CityId,
+                name = c.Name,
+            }).Last();
         }
 
     }
