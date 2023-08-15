d8956b4e568847f47b101e33d3c3082902c866eb
Thiago Lira <thiago.slira@outlook.com>
2023-08-15 16:50:06 -0300

    <chore>: Phase A RoomRepository implementation completed

--- src/TrybeHotel/Repository/RoomRepository.cs
+++ src/TrybeHotel/Repository/RoomRepository.cs
@@ -11,16 +11,64 @@ namespace TrybeHotel.Repository
             _context = context;
         }
 
-        public IEnumerable<RoomDto> GetRooms(int HotelId) {
-            throw new NotImplementedException();
+        public IEnumerable<RoomDto> GetRooms(int HotelId)
+        {
+            var content = from room in _context.Rooms
+                          join hotel in _context.Hotels on room.HotelId equals hotel.HotelId
+                          join city in _context.Cities on hotel.CityId equals city.CityId
+                          where hotel.HotelId == HotelId
+                          select new RoomDto
+                          {
+                              roomId = room.RoomId,
+                              name = room.Name,
+                              capacity = room.Capacity,
+                              image = room.Image,
+                              hotel = new HotelDto
+                              {
+                                  hotelId = hotel.HotelId,
+                                  name = hotel.Name,
+                                  address = hotel.Address,
+                                  cityId = city.CityId,
+                                  cityName = city.Name
+                              }
+                          };
+            return content;
         }
 
-        public RoomDto AddRoom(Room room) {
-           throw new NotImplementedException();
+        public RoomDto AddRoom(Room room)
+        {
+            _context.Rooms.Add(room);
+            _context.SaveChanges();
+            var content = from r in _context.Rooms
+                          join h in _context.Hotels on r.HotelId equals h.HotelId
+                          join c in _context.Cities on h.CityId equals c.CityId
+                          where r.RoomId == room.RoomId
+                          select new RoomDto
+                          {
+                              roomId = r.RoomId,
+                              name = r.Name,
+                              capacity = r.Capacity,
+                              image = r.Image,
+                              hotel = new HotelDto
+                              {
+                                  hotelId = h.HotelId,
+                                  name = h.Name,
+                                  address = h.Address,
+                                  cityId = c.CityId,
+                                  cityName = c.Name
+                              }
+                          };
+            return content.Last();
         }
 
-        public void DeleteRoom(int RoomId) {
-            throw new NotImplementedException();
+        public void DeleteRoom(int RoomId)
+        {
+            var roomToRemove = _context.Rooms.Find(RoomId);
+            if (roomToRemove != null)
+            {
+                _context.Rooms.Remove(roomToRemove);
+                _context.SaveChanges();
+            }
         }
     }
 }
