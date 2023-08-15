4bf6e201b7e3697e480ee68ed9c9b986e162f5b8
Thiago Lira <thiago.slira@outlook.com>
2023-08-15 18:34:40 -0300

    <feat>: Phase B UserDtoInsert implemented

--- src/TrybeHotel/Dto/UserDto.cs
+++ src/TrybeHotel/Dto/UserDto.cs
@@ -1,14 +1,20 @@
 namespace TrybeHotel.Dto
 {
-    public class UserDto {
+    public class UserDto
+    {
 
     }
 
-    public class UserDtoInsert {
-       
+    public class UserDtoInsert
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
     }
 
-    public class LoginDto {
-      
+    public class LoginDto
+    {
+
     }
 }
