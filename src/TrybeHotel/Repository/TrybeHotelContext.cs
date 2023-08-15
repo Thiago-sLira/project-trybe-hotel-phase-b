c522ab8ddf7210bbd9a16fe974701a17bf6f43f0
Thiago Lira <thiago.slira@outlook.com>
2023-08-15 16:39:08 -0300

    <chore>: Phase A TrybeHotelContext implementation completed

--- src/TrybeHotel/Repository/TrybeHotelContext.cs
+++ src/TrybeHotel/Repository/TrybeHotelContext.cs
@@ -4,13 +4,41 @@ using TrybeHotel.Models;
 namespace TrybeHotel.Repository;
 public class TrybeHotelContext : DbContext, ITrybeHotelContext
 {
-    public TrybeHotelContext(DbContextOptions<TrybeHotelContext> options) : base(options) {
+    public TrybeHotelContext(DbContextOptions<TrybeHotelContext> options) : base(options)
+    {
         Seeder.SeedUserAdmin(this);
     }
     public TrybeHotelContext() { }
-    
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {}
 
-    protected override void OnModelCreating(ModelBuilder modelBuilder) {}
+    public DbSet<City> Cities { get; set; } = null!;
+
+    public DbSet<Hotel> Hotels { get; set; } = null!;
+
+    public DbSet<Room> Rooms { get; set; } = null!;
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+        optionsBuilder.UseSqlServer
+        (
+            @"
+            Server=localhost;
+            Database=TrybeHotel;
+            User=SA;
+            Password=TrybeHotel12!;
+            TrustServerCertificate=True
+            "
+        );
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder) {
+        // modelBuilder.Entity<Hotel>()
+        // .HasOne(h => h.City)
+        // .WithMany(c => c.Hotels)
+        // .HasForeignKey(h => h.CityId);
+
+        // modelBuilder.Entity<Room>()
+        // .HasOne(r => r.Hotel)
+        // .WithMany(h => h.Rooms)
+        // .HasForeignKey(r => r.HotelId);
+    }
 
 }
