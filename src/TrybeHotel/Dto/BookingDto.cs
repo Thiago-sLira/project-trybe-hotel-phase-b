6b21111ccd4037acb4fb8a51127e9dd5a37881d2
Thiago Lira <thiago.slira@outlook.com>
2023-08-17 17:09:02 -0300

    <feat>: Phase B BookingDto => Implemented BookingDtoInsert and BookingResponse

--- src/TrybeHotel/Dto/BookingDto.cs
+++ src/TrybeHotel/Dto/BookingDto.cs
@@ -2,9 +2,18 @@ namespace TrybeHotel.Dto
 {
     public class BookingDtoInsert
     {
+        public string CheckIn { get; set; }
+        public string CheckOut { get; set; }
+        public string GuestQuant { get; set; }
+        public int RoomId { get; set; }
     }
 
     public class BookingResponse
     {
+        public int bookingId { get; set; }
+        public string CheckIn { get; set; }
+        public string CheckOut { get; set; }
+        public string guestQuant { get; set; }
+        public RoomDto room { get; set; }
     }
 }
